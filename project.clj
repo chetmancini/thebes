63508160db72b8d870d8a16b02c5f2080071c542
Chet Mancini <chetmancini@gmail.com>
2013-12-24 11:34:24 -0500

    CM: Add cascalog to project

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject thebes "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A lambda architecture for realtime data processing"
+  :url "http://github.com/chetmancini/thebes"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :repositories {"conjars" "http://conjars.org/repo"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [cascalog/cascalog-core "2.0.0"]]
   :main ^:skip-aot thebes.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:dev {:dependencies [[org.apache.hadoop/hadoop-core "1.1.2"]]}
+             :uberjar {:aot :all}})
