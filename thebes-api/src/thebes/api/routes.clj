73415fe166a0bdc8ceef44fc5c811951a9c11208
Chet Mancini <chetmancini@gmail.com>
2013-12-24 15:21:42 -0500

    CM: post api working

--- thebes-api/src/thebes/api/routes.clj
+++ thebes-api/src/thebes/api/routes.clj
@@ -7,13 +7,27 @@
         [compojure.core :only [context ANY routes defroutes]]
         [clojure.string :only [split]]))
 
+(defn put-data [input]
+  (println input))
+
 (defresource hello-world
   :handle-ok "Hello World!"
   :etag "fixed-etag"
   :available-media-types ["text/plain"])
 
+(defresource data-input
+  :allowed-methods [:post :get]
+  :available-media-types ["application/json"]
+  :handle-ok (fn [ctx]
+              (str "<html>Post application/json to this resource.</html>"))
+  :post! (fn [ctx]
+           (dosync 
+            (let [body (slurp (get-in ctx [:request :body]))]
+              (put-data body)))))
+
 (defn assemble-routes []
   (->
    (routes
+    (ANY "/api/v1/data/in" [] data-input)
     (ANY "/hello-world" [] hello-world))
    (dev/wrap-trace :ui :header)))
