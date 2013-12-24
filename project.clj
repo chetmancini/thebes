3918c93f5929bffc12bfde87a5ba77f517126e5e
Chet Mancini <chetmancini@gmail.com>
2013-12-24 11:37:13 -0500

    CM: add Storm dependency

--- project.clj
+++ project.clj
@@ -4,8 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"conjars" "http://conjars.org/repo"}
+  :jvm-opts ["-Djava.library.path=/usr/local/lib:/opt/local/lib:/usr/lib"]
+  :javac-options {:debug "true" :fork "true"}
+  :java-source-path "src/jvm"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [cascalog/cascalog-core "2.0.0"]]
+                 [cascalog/cascalog-core "2.0.0"]
+                 [storm "0.9.0.1"]]
   :main ^:skip-aot thebes.core
   :target-path "target/%s"
   :profiles {:dev {:dependencies [[org.apache.hadoop/hadoop-core "1.1.2"]]}
