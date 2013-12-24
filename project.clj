bbd4952d1bde30cc1466b85a9b17059afe0b34c3
Chet Mancini <chetmancini@gmail.com>
2013-12-24 13:22:04 -0500

    CM: add liberator

--- project.clj
+++ project.clj
@@ -3,16 +3,22 @@
   :url "http://github.com/chetmancini/thebes"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:connection "scm:git:https://github.com/chetmancini/thebes.git"
+        :url "https://github.com/chetmancini/thebes"}
   :repositories {"conjars" "http://conjars.org/repo"}
   :jvm-opts ["-Djava.library.path=/usr/local/lib:/opt/local/lib:/usr/lib"]
   :javac-options {:debug "true" :fork "true"}
   :java-source-path "src/jvm"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [cascalog/cascalog-core "2.0.0"]
-                 [storm "0.9.0.1"]]
+                 [storm "0.9.0.1"]
+                 [liberator "0.10.0"]]
   :aot [thebes.TopologySubmitter]
   :main ^:skip-aot thebes.core
   :target-path "target/%s"
+  :source-paths ["src"]
+  :test-paths ["test"]
+  :aliases {"hello-world" ["run" "-m" "thebes.api.hello-world"]}
   :profiles {:dev {:dependencies [[org.apache.hadoop/hadoop-core "1.1.2"]
                                   [lein-midje "3.0.1"]
                                   [cascalog/midje-cascalog "2.0.0"]
---
+++ src/thebes/api/helloworld.clj
@@ -0,0 +1,6 @@
+(ns thebes.api.hello-world
+  (:use [liberator.core :only [defresource]]))
+
+(defresource hello-world
+  :available-media-types ["text/plain"]
+  :handle-ok "Hello, world!")
